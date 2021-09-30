92f225669a2a1a47399811f7a86ae77ef15dfcdf
zenliarth <54330804+zenliarth@users.noreply.github.com>
2021-09-30 19:42:09 -0300

    first commit todolist

--- src/App.js
+++ src/App.js
@@ -1,23 +1,71 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
+  const [value, setValue] = React.useState('');
+  const [list, setList] = React.useState([]);
+  const [count, setCount] = React.useState(0);
+
+  window.localStorage.setItem('Lista', list);
+
+  const handleAdd = () => {
+    if (value === '') {
+      alert('Campo obrigatório!');
+    } else if (list.includes(value)) {
+      alert('Item já adicionado.');
+      setValue('');
+    } else {
+      setList([...list, value]);
+      setCount(count >= 0 ? count + 1 : count);
+      setValue('');
+    }
+  };
+  const handleRemove = (index) => {
+    const lista = [...list];
+    const listaFilter = lista.filter((item, i) => item[i] !== item[index]); // segunda forma com filter
+    setList(listaFilter);
+    setCount(count >= 0 ? count - 1 : count);
+    /* setList([...list, list.splice(index, 1)]); // Primeira forma que fiz.
+    setList([...list]); */
+  };
+
+  const handleDelete = () => {
+    setList([]);
+    setCount(0);
+    setValue('');
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="boxToDo">
+        <h1>Minha Lista</h1>
+        <form onSubmit={(e) => e.preventDefault()}>
+          <label htmlFor="/">
+            <input
+              type="text"
+              value={value}
+              onChange={(e) => setValue(e.target.value)}
+            />
+          </label>
+          <button type="submit" onClick={handleAdd}>
+            add
+          </button>
+        </form>
+      </div>
+      <div className="list">
+        <ul>
+          {list.map((item, index) => (
+            <li key={index}>
+              {item}
+              <button onClick={() => handleRemove(index)}>X</button>
+            </li>
+          ))}
+        </ul>
+      </div>
+      <div className="boxContador">
+        <p>Total Tasks: {count}</p>
+        <button onClick={handleDelete}>Limpar Lista</button>
+      </div>
     </div>
   );
 }
